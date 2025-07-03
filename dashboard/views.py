67d184e7166507526574894317df6714c77b781d
hoanghuan23 <huanhoang2003@gmail.com>
2025-07-03 11:24:09 +0700

    thay đổi cấu trúc project (bỏ bản web)

--- dashboard/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import redirect, render
-from zalo_crawler.zalo_fetcher import start_crawling
-
-def dashboard_view(request):
-    if request.method == "POST":
-        start_crawling()
-        return redirect('templates')
-    return render(request, 'trangchu.html')
