7f22e52ffe2ba76b587a354271717bbd8dcd172a
hoanghuan23 <huanhoang2003@gmail.com>
2025-08-06 09:51:38 +0700

    zalo_fetcher + openai craw dữ liệu tự động + xử lý

--- openaitool.py
+++ openaitool.py
@@ -1,17 +1,18 @@
 from openai import OpenAI
-from meta_schema import CAREER_NOTE
-from meta_schema import META_JOB_SCHEMA
+from meta_schema import META_JOB_SCHEMA, CAREER_NOTE
+from meta_schema_uv import META_UV_SCHEMA
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-def analyzeAndSplitJobContent(groupName, rawText):
+def analyzeAndSplitJobContent(rawText):
     chat_completion = client.chat.completions.create(
         messages=[
             {
                 "role": "system",
                 "content": (
-                    "Bối cảnh: " + groupName + ".\n" +
                     "- Tuyệt đối giữ nguyên nội dung tin nhắn gốc, chỉ chuẩn hóa lại cách viết để dễ đọc và phân tích chính xác hơn.\n" +
                     "- Không được thêm bất kỳ từ, cụm từ, hay nội dung AI nào không có trong tin nhắn gốc.Nếu có từ tiếng nhật thì dịch chuẩn\n" +
                     "- Chỉ thực hiện chuẩn hóa các từ viết tắt, lỗi chính tả, ký tự đặc biệt, hoặc cách ngắt chữ không tự nhiên.\n" +
@@ -26,26 +27,21 @@ def analyzeAndSplitJobContent(groupName, rawText):
                     "- Bắt buộc phải trả về giá trị, không để trống.")
             },
             {
-                "role": "user",
+                "role": "system",
                 "content": rawText,
             }
         ],
-        model="gpt-4o",
+        model="gpt-4.1",
     )
     return chat_completion
 
 
-def analyzeJobInformation(groupName, sender, rawText):
+def analyzeJobInformation(rawText):
     chat_completion = client.chat.completions.create(
         response_format={
             "type": "json_schema",
             "json_schema": META_JOB_SCHEMA
         },
-        # temperature=1,
-        # max_completion_tokens=2048,
-        # top_p=1,
-        # frequency_penalty=0,
-        # presence_penalty=0,
         messages=[
             {
                 "role": "system",
@@ -53,18 +49,45 @@ def analyzeJobInformation(groupName, sender, rawText):
                     "Bạn là chuyên gia phân tích dữ liệu có 20 năm kinh nghiệm"
                     "Nhiệm vụ của bạn là phân tích nội dung đầu vào và xác định đúng loại visa bằng cách kiểm tra đầy đủ các tiêu chí:"
                     "\n Trước khi phân loại, bạn phải kiểm tra **tất cả từ khóa liên quan** trong nội dung."
-                    "\n Nếu một nội dung chứa từ khóa liên quan đến **nhiều loại visa khác nhau**, hãy ưu tiên loại visa có **nhiều từ khóa phù hợp hơn**."
                     "\n Nếu một nội dung có tỉnh thành của Nhật Bản, điều đó không có nghĩa là nó là 'đầu Nhật' trừ khi có từ khóa liên quan đến loại visa."
                     "\n không tự động gán loại visa nếu không có thông tin cụ thể."
                     "\nTrả về kết quả JSON theo định dạng yêu cầu. Trả về đúng kết quả schema và không thêm bất kỳ thông tin dư thừa nào")
             },
 
             {
-                "role": "user",
+                "role": "system",
                 "content": 'Phân tích nội dung: "'+rawText+'"',
             }
         ],
-        model="gpt-4o",
+        model="gpt-4.1",
     )
     return chat_completion
 
+def analysisPostType(rawText):
+    chat_completion = client.chat.completions.create(
+        response_format={
+            "type": "json_schema",
+            "json_schema": META_UV_SCHEMA
+        },
+        messages=[
+            {
+                "role": "system",
+                "content": (
+                    """
+                    "content": (
+                        "Phân loại nội dung thành một trong ba loại: 'VIỆC LÀM NHẬT', 'ỨNG VIÊN' hoặc 'TIN RÁC' dựa vào {META_UV_SCHEMA}\n"
+                        "Nếu có nhiều đơn hàng trong một ô nội dung phân loại là 'TIN RÁC'\n"
+                        "Các đơn hàng không thuộc nước Nhật Bản , đơn Đài Loan, Hàn quốc... và nội dung không rõ ràng phân loại là 'TIN RÁC'\n"
+                        "Trả về JSON đúng định dạng schema, không thêm bất kỳ nội dung nào ngoài kết quả."
+                    )                    
+                    """
+                )
+            },
+            {
+                "role": "system",
+                "content": 'Phân tích nội dung: "'+rawText+'"',
+            }
+        ],
+        model="gpt-4.1",
+    )
+    return chat_completion
