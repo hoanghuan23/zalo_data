d3bfc4a5611de1c7305580f1ace47e39e1d013a9
hoanghuan23 <huanhoang2003@gmail.com>
2025-08-06 09:40:52 +0700

    chương trình crawl bài viết face và lưu cookie

---
+++ luu_cookie.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pickle
+import time
+
+driver = webdriver.Chrome()
+url = "https://www.facebook.com/"
+driver.get(url)
+time.sleep(60)
+
+try:
+    cookies = driver.get_cookies()
+    pickle.dump(cookies, open("facebook_cookies_02.pkl", "wb"))
+    print("Cookie đã được lưu.")
+
+finally:
+    driver.quit()
